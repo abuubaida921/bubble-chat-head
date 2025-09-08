c02a4fafb910edaae2e5090c1011d52e3cdc5ed3
Abu Ubaida <ubaidanazia@gmail.com>
2025-09-08 16:09:20 +0600

    Init

---
+++ app/src/main/java/com/abuubaida921/bubble_chat_head/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.abuubaida921.bubble_chat_head
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.abuubaida921.bubble_chat_head.ui.theme.Bubble_Chat_HeadTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Bubble_Chat_HeadTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Bubble_Chat_HeadTheme {
+        Greeting("Android")
+    }
+}
